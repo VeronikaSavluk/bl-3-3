4c6b4d7ffd847cccc9a179cb3d6ee02999ae8e76
Nataliya Pogrebniak <pogrebnyak.nataliya@gmail.com>
2022-10-29 12:07:04 +0300

    add countryInfo

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,33 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from '../service/country-service';
 import { Section, Container, CountryInfo, Loader } from 'components';
 
 export const Country = () => {
+  const [country, setCountry] = useState([]);
+  const { countryId } = useParams();
+  useEffect(() => {
+    fetchCountry(countryId).then(data => setCountry(data));
+  });
+  const {
+    flag,
+    capital,
+    countryName,
+    id,
+    languages = [],
+    population,
+  } = country;
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <CountryInfo
+          flag={flag}
+          capital={capital}
+          country={countryName}
+          id={id}
+          languages={languages}
+          population={population}
+        />
       </Container>
     </Section>
   );
